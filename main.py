fa70a52a44864389870e9a834f035c925375ce16
Andrew Morgan <andrew@amorgan.xyz>
2018-12-12 23:01:18 +0100

    Make channels more efficient

--- main.py
+++ main.py
@@ -21,13 +21,17 @@ reddit = praw.Reddit(client_id=settings["reddit_client_id"],
 # Wait for discord client to be ready
 @client.event
 async def on_ready():
+    channels = []
+    for channel_id in settings["channels"]:
+        channels.append(await client.get_channel(channel_id))
+
     for submission in reddit.subreddit('mlpdrawingschool').stream.submissions():
         if re.match("\[Weekly Challenge\] .*", submission.title):
             post_text = ("# Hey bois n girls, it's time for a new weekly challenge!\n\n" +
                         submission.selftext + "\n\nLink: " + 
                         "https://reddit.com" + submission.permalink)
 
-            for channel in settings["channels"]:
-                await client.send_message(client.get_channel(channel), post_text)
+            for channel in channels:
+                await client.send_message(channel, post_text)
 
 client.run(settings["discord_client_token"])
