5ead24708e3ce23a64917a8ba5e017996253eb97
Andrew Morgan <andrew@amorgan.xyz>
2018-12-12 22:52:32 +0100

    Make things work

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
 from matrix_client.api import MatrixHttpApi
+
+import discord
+import asyncio
 import praw
 import re
 import json
 
+# Open settings file
 with open("settings.json") as s:
     settings = json.loads(s.read())
 
-# Setup matrix auth
-matrix = MatrixClient("http://amorgan.xyz", token=settings["matrix_token"])
-
-token = client.login(username=settings["matrix_username"],
-                     password=settings["matrix_password"])
+# Setup discord auth
+client = discord.Client()
 
 # Setup reddit auth
 reddit = praw.Reddit(client_id=settings["reddit_client_id"],
                      client_secret=settings["reddit_client_secret"],
                      user_agent=settings["reddit_user_agent"])
 
-for submission in reddit.subreddit('mlpdrawingschool').stream.submissions():
-    if re.match("\[Weekly Challenge\] .*", submission.title):
-        post_text = ("# Hey bois n girls, it's time for a new weekly challenge!\n\n" +
-                    submission.selftext + "\n\nLink: " + submission.permalink)
+# Wait for discord client to be ready
+@client.event
+async def on_ready():
+    for submission in reddit.subreddit('mlpdrawingschool').stream.submissions():
+        if re.match("\[Weekly Challenge\] .*", submission.title):
+            post_text = ("# Hey bois n girls, it's time for a new weekly challenge!\n\n" +
+                        submission.selftext + "\n\nLink: " + 
+                        "https://reddit.com" + submission.permalink)
 
-        for room_id in settings["rooms"]:
-            print(matrix.send_message(room_id, post_text))
+            for channel in settings["channels"]:
+                await client.send_message(client.get_channel(channel), post_text)
+
+client.run(settings["discord_client_token"])
